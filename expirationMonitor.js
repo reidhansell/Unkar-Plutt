e2c8352be430bab1d004ef78313475178e0c80c9
Reid Hansell <reidhansell@gmail.com>
2023-12-28 06:56:42 -0700

    add logging

--- expirationMonitor.js
+++ expirationMonitor.js
@@ -3,12 +3,14 @@ import Contract from './objects/Contract.js';
 
 export async function markExpiredContracts() {
     try {
+        console.log("Marking expired contracts...");
         const expiredContracts = await getExpiredContracts();
 
         for (let contractData of expiredContracts) {
             let contract = new Contract(JSON.parse(contractData.contract_object));
             contract.setStatus("EXPIRED");
             await updateContract(contract);
+            console.log(`Marked contract ${contract.getContractID()} as EXPIRED`);
         }
     } catch (error) {
         console.error("Error marking contracts as expired:", error);
